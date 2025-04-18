408ad86081cb98873e4aa2633f6b38d025497b0d
vigneshwaran <130039318+Vigneshwaran-01@users.noreply.github.com>
2025-04-18 09:45:05 +0530

    Update 2667_createHelloWorld.js

--- Day-01/2667_createHelloWorld.js
+++ Day-01/2667_createHelloWorld.js
@@ -2,3 +2,15 @@
 // Link: https://leetcode.com/problems/create-hello-world-function/
 // Language: JavaScript
 // Difficulty: Easy
+
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        return "Hello World"
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
